ef9f9305f088877c05f884113d580083ad2836d3
eedyGarcia <eedy.ega@gmail.com>
2022-05-21 14:40:28 -0500

    u20220521-1

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var app = angular.module("miApp",[]);
+var app = angular.module("myApp",[]);
 
-app.controller("MiAppController", function(){
+app.controller("nombresAppController", function(){
     var mac = this;
 
     if(localStorage.getItem('apellidos')){
@@ -21,9 +21,39 @@ app.controller("MiAppController", function(){
         if(mac.nuevoApellido.length){
             if(!mac.apellidos.includes(mac.nuevoApellido)){
                 mac.apellidos.push(mac.nuevoApellido);
-                localStorage.setItem('apellidos') = JSON.stringify(mac.apellidos);
+                localStorage.setItem('apellidos',JSON.stringify(mac.apellidos));
             }
             mac.nuevoApellido='';
         }
     }
+})
+
+.controller('gifsAppController', function($scope,$http){
+    var gac = this;
+        gac.resultados;
+
+    var baseUrl = 'https://api.giphy.com/v1/gifs/search?';
+    
+    var params = [];
+        params.api_key = 'AFWyHuxkaMBGUqkAmauon984HsFrBb8p';
+        params.limit = '20';
+        
+    gac.buscarGifs = ()=>{
+        params.q = gac.busqueda;
+        
+        params = HttpParams(params);
+        gac.uri = baseUrl+params;
+
+        $http.get(gac.uri).success(function(resp){
+            gac.resultados = resp.data;
+        });
+    };
 });
+
+
+const HttpParams = (data)=>{
+    const ret = [];
+    for (let d in data)
+      ret.push(encodeURIComponent(d) + '=' + encodeURIComponent(data[d]));
+    return ret.join('&');
+};
