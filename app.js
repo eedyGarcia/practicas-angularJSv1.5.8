53b392bf38c0a7b9d62d8deb44d7bdfdb6647ba5
eedyGarcia <eedy.ega@gmail.com>
2022-05-20 20:12:41 -0500

    u20220520-1

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
 var app = angular.module("miApp",[]);
 
-app.controller("MiAppController", function($scope){
-    $scope.nombre="Eduardo";
-    $scope.apellido="López";
+app.controller("MiAppController", function(){
+    var mac = this;
+
+    if(localStorage.getItem('apellidos')){
+        mac.apellidos = JSON.parse(localStorage.getItem('apellidos'));
+    }else{
+        mac.apellidos = ['López','Reyes', 'Martínez', 'Sánchez', 'García'];
+    }
+    
+
+    mac.nombre="Eduardo";
+    mac.apellido="López";
+
+    mac.cambiarTexto = ()=>{
+        mac.apellido="García";
+    }
+
+    mac.agregarApellido =()=>{
+        if(mac.nuevoApellido.length){
+            if(!mac.apellidos.includes(mac.nuevoApellido)){
+                mac.apellidos.push(mac.nuevoApellido);
+                localStorage.setItem('apellidos') = JSON.stringify(mac.apellidos);
+            }
+            mac.nuevoApellido='';
+        }
+    }
 });
